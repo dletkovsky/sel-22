c6c9be3b6f4501f90f9e7b43dd0d18006a405995
d_letkovskiy <d_letkovskiy@wargaming.net>
2018-09-26 16:00:52 +0300

    add LitecartLoginTest

---
+++ SeleniumProj/SeleniumProj/litecart/BaseTestCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumProj.litecart
+{
+    public class BaseTestCase
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("c:\\Users\\d_letkovskiy\\Downloads");
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumProj/SeleniumProj/litecart/LitecartLoginTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumProj.litecart
+{
+    public class LitecartLoginTest : BaseTestCase
+    {
+        private const string USERNAME_INPUT_XPATH = "//input[@name='username']";
+        private const string PASSWORD_INPUT_XPATH = "//input[@name='password']";
+        private const string LOGIN_BUTTON_XPATH = "//button[@name='login']";
+        private const string LOGOUT_BUTTON_XPATH = "//a[@href='http://localhost/litecart/admin/logout.php']";
+
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.XPath(USERNAME_INPUT_XPATH)).SendKeys("admin");
+            driver.FindElement(By.XPath(PASSWORD_INPUT_XPATH)).SendKeys("admin");
+            driver.FindElement(By.XPath(LOGIN_BUTTON_XPATH)).Click();
+            Assert.True(driver.FindElements(By.XPath(LOGOUT_BUTTON_XPATH)).Count > 0);
+        }
+    }
+}
