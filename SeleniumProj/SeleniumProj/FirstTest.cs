e601809a25013d0cda25ecf58a9433324030ae4d
d_letkovskiy <d_letkovskiy@wargaming.net>
2018-09-26 13:25:54 +0300

    add proj

---
+++ SeleniumProj/SeleniumProj/FirstTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumProj
+{
+    [TestFixture]
+    [Description("First webdriver test")]
+    public class FirstTest
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("c:\\Users\\d_letkovskiy\\Downloads");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
