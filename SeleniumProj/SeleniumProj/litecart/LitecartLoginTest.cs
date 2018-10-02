1d8848e7026e9e227697e5c921e0c26a9215cde4
d_letkovskiy <d_letkovskiy@wargaming.net>
2018-10-02 13:34:37 +0300

    task 10

--- SeleniumProj/SeleniumProj/litecart/LitecartLoginTest.cs
+++ SeleniumProj/SeleniumProj/litecart/LitecartLoginTest.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using SeleniumProj.litecart.objects;
+using SeleniumProj.litecart.utils;
 using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
 
 namespace SeleniumProj.litecart
@@ -219,7 +224,6 @@ namespace SeleniumProj.litecart
         [Test]
         public void LoginTest_task8()
         {
-            //1) входит в панель администратора http://localhost/litecart/admin
             openMainPageLitecart();
 
             foreach (var box in new List<string> {"box-most-popular", "box-campaigns", "box-latest-products"})
@@ -254,7 +258,8 @@ namespace SeleniumProj.litecart
             var listCountries = new List<string>();
             for (var i = 2; i < countCountries; i++)
             {
-                listCountries.Add(driver.FindElement(By.XPath($"//form[@name='countries_form']//tbody/tr[{i}]/td[5]")).Text);
+                listCountries.Add(driver.FindElement(By.XPath($"//form[@name='countries_form']//tbody/tr[{i}]/td[5]"))
+                    .Text);
                 if (driver.FindElement(By.XPath($"//form[@name='countries_form']//tbody/tr[{i}]/td[6]")).Text != "0")
                 {
                     listCountriesWithZones.Add(i);
@@ -264,7 +269,6 @@ namespace SeleniumProj.litecart
             var expectedList = listCountries.OrderBy(s => s);
             verifyUtils.verifyTrue(listCountries.SequenceEqual(expectedList), "Некорретная сортировка стран!");
 
-            
 
             if (listCountriesWithZones.Count != 0)
             {
@@ -275,13 +279,16 @@ namespace SeleniumProj.litecart
                     var zonesList = new List<string>();
                     for (var i = 2; i < count; i++)
                     {
-                        zonesList.Add(driver.FindElement(By.XPath($"//table[@id='table-zones']/tbody/tr[{i}]/td[3]")).Text);
+                        zonesList.Add(driver.FindElement(By.XPath($"//table[@id='table-zones']/tbody/tr[{i}]/td[3]"))
+                            .Text);
                     }
+
                     var expectedZonesList = zonesList.OrderBy(s => s);
                     verifyUtils.verifyTrue(zonesList.SequenceEqual(expectedZonesList), "Некорретная сортировка zones!");
                     driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
                 }
             }
+
             verifyUtils.checkForVerifications();
         }
 
@@ -299,7 +306,8 @@ namespace SeleniumProj.litecart
             var listCountries = new List<string>();
             for (var i = 2; i < countCountries; i++)
             {
-                listCountries.Add(driver.FindElement(By.XPath($"//form[@name='geo_zones_form']//tbody/tr[{i}]/td[3]")).Text);
+                listCountries.Add(driver.FindElement(By.XPath($"//form[@name='geo_zones_form']//tbody/tr[{i}]/td[3]"))
+                    .Text);
             }
 
             var expectedList = listCountries.OrderBy(s => s);
@@ -308,19 +316,141 @@ namespace SeleniumProj.litecart
 
             for (int j = 2; j < countCountries; j++)
             {
-                    driver.FindElement(By.XPath($"//form[@name='geo_zones_form']//tbody/tr[{j}]/td[3]/a")).Click();
+                driver.FindElement(By.XPath($"//form[@name='geo_zones_form']//tbody/tr[{j}]/td[3]/a")).Click();
 
-                    var count = driver.FindElements(By.XPath("//table[@id='table-zones']/tbody/tr")).Count;
-                    var zonesList = new List<string>();
-                    for (var i = 2; i < count; i++)
-                    {
-                        zonesList.Add(driver.FindElement(By.XPath($"//table[@id='table-zones']/tbody/tr[{i}]/td[3]/select/option[@selected]")).Text);
-                    }
-                    var expectedZonesList = zonesList.OrderBy(s => s);
-                    verifyUtils.verifyTrue(zonesList.SequenceEqual(expectedZonesList), "Некорретная сортировка zones!");
+                var count = driver.FindElements(By.XPath("//table[@id='table-zones']/tbody/tr")).Count;
+                var zonesList = new List<string>();
+                for (var i = 2; i < count; i++)
+                {
+                    zonesList.Add(driver
+                        .FindElement(
+                            By.XPath($"//table[@id='table-zones']/tbody/tr[{i}]/td[3]/select/option[@selected]")).Text);
+                }
+
+                var expectedZonesList = zonesList.OrderBy(s => s);
+                verifyUtils.verifyTrue(zonesList.SequenceEqual(expectedZonesList), "Некорретная сортировка zones!");
                 driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
             }
+
             verifyUtils.checkForVerifications();
+        }
+
+
+        [Test]
+        public void LoginTest_task10()
+        {
+            openMainPageLitecart();
+            var productMainPage = getCompaignsProductOnMainPage(1);
+
+            driver.FindElement(By.XPath(CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN)).Click();
+            var product = getCompaignsProductPageProduct();
+
+
+            //проверка основных значений продукта
+            verifyUtils.verifyTrue(productMainPage.Equals(product),
+                "Значения полей товара на главной странице и странице товара различны!");
+
+            //проверка размеров цен
+            verifyUtils.verifyTrue(productMainPage.sizeCampPrice.Width < product.sizeCampPrice.Width,
+                "Ширина акционной цены не соответствует требованиям!");
+            verifyUtils.verifyTrue(productMainPage.sizeCampPrice.Height < product.sizeCampPrice.Height,
+                "Высота акционной цены не соответствует требованиям!");
+            verifyUtils.verifyTrue(productMainPage.sizeRegPrice.Width < product.sizeRegPrice.Width,
+                "Ширина обычной цены не соответствует требованиям!");
+            verifyUtils.verifyTrue(productMainPage.sizeRegPrice.Height < product.sizeRegPrice.Height,
+                "Высота обычной цены не соответствует требованиям!");
+
+            //проверка цвета акционной цены
+            verifyUtils.verifyTrue(productMainPage.colorCampPrice.Equals(product.colorCampPrice),
+                "Цвет акционной цены на главной странице и странице товара различны!");
+
+            //проверка цветов обычной цены
+            verifyUtils.verifyTrue(productMainPage.isRGBEqualColorRegPrice(), "Цвет обычной цены на главной странице некорректен!");
+            verifyUtils.verifyTrue(product.isRGBEqualColorRegPrice(), "Цвет обычной цены на странице товара некорректен!");
+            verifyUtils.checkForVerifications();
+        }
+
+
+        public const string CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN =
+            "//div[@id='box-campaigns']//ul[@class='listing-wrapper products']/li";
+
+        public const string CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN =
+            "//div[@id='box-product']";
+
+
+        
+
+        public Product getCompaignsProductOnMainPage(int index)
+        {
+            Product product = new Product
+            {
+                name = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN + $"[{index}]//div[@class='name']"))
+                    .Text,
+                reg_price = driver.FindElement(By.XPath(
+                    CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN +
+                    $"[{index}]//div/s[@class='regular-price']")).Text,
+                campaign_price = driver.FindElement(By.XPath(
+                    CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN +
+                    $"[{index}]//div/strong[@class='campaign-price']")).Text,
+                colorCampPrice = Utils.ParseColor(driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN +
+                                          $"[{index}]//div/strong[@class='campaign-price']"))
+                    .GetCssValue("color")),
+                colorRegPrice = Utils.ParseColor(driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN +
+                                          $"[{index}]//div/s[@class='regular-price']"))
+                    .GetCssValue("color")),
+                sizeCampPrice = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN +
+                                          $"[{index}]//div/strong[@class='campaign-price']"))
+                    .Size,
+                sizeRegPrice = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_MAIN_PAGE_XPATH_PATTERN +
+                                          $"[{index}]//div/s[@class='regular-price']"))
+                    .Size
+            };
+            return product;
+        }
+
+
+        public Product getCompaignsProductPageProduct()
+        {
+            var product = new Product
+            {
+                name = driver
+                    .FindElement(
+                        By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN + "//h1[@class='title']"))
+                    .Text,
+                reg_price = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN +
+                                          "//s[@class='regular-price']"))
+                    .Text,
+                campaign_price = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN +
+                                          "//strong[@class='campaign-price']")).Text,
+
+
+                colorCampPrice = Utils.ParseColor(driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN +
+                                          "//strong[@class='campaign-price']"))
+                    .GetCssValue("color")),
+
+                colorRegPrice = Utils.ParseColor(driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN +
+                                          "//s[@class='regular-price']"))
+                    .GetCssValue("color")),
+
+                sizeCampPrice = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN +
+                                          "//strong[@class='campaign-price']"))
+                    .Size,
+
+                sizeRegPrice = driver
+                    .FindElement(By.XPath(CAMPAIGNS_PRODUCT_PRODUCT_PAGE_XPATH_PATTERN + "//s[@class='regular-price']"))
+                    .Size
+            };
+            return product;
         }
     }
 }
---
+++ SeleniumProj/SeleniumProj/litecart/objects/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProj.litecart.objects
+{
+    public class Product
+    {
+        public string name { get; set; }
+
+        public string campaign_price { get; set; }
+        public string reg_price { get; set; }
+
+
+        public Color colorCampPrice { get; set; }
+        public Color colorRegPrice { get; set; }
+
+
+        public Size sizeCampPrice { get; set; }
+        public Size sizeRegPrice { get; set; }
+
+
+        public bool isRGBEqualColorRegPrice() =>
+            colorRegPrice.R.Equals(colorRegPrice.B) && colorRegPrice.R.Equals(colorRegPrice.G);
+
+
+        protected bool Equals(Product other)
+        {
+            return string.Equals(name, other.name) && string.Equals(campaign_price, other.campaign_price) &&
+                   string.Equals(reg_price, other.reg_price);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Product) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (name != null ? name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (campaign_price != null ? campaign_price.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (reg_price != null ? reg_price.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Product left, Product right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Product left, Product right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
